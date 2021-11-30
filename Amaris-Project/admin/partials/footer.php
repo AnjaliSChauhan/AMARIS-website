165309266f4c7185938823afc2f3771e65df950d
Anjali C <86975210+AnjaliSChauhan@users.noreply.github.com>
2021-11-30 22:31:40 +0530

    Add files via upload

---
+++ Amaris-Project/admin/partials/footer.php
@@ -0,0 +1,34 @@
+<!-- footer section -->
+<section class="social">
+
+<div class="container text-center">
+    <ul>
+        <li>
+            <a href="https://twitter.com/"  target="_blank" ><i class="fab fa-twitter  fa-3x"></i></a>
+        </li>
+        <li>
+            <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram fa-3x"></i></a>
+        </li>
+        <li>
+            <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook fa-3x"></i></a>
+        </li>
+        <li>
+            <a href="https://www.gmail.com/" target="_blank"><i class="fas fa-envelope fa-3x"></i></a>
+        </li>
+    </ul>
+</div>
+</section> 
+<!-- social Section Ends Here -->
+
+<!-- footer Section Starts Here -->
+<section class="footer">
+<div class="container text-center ">
+    <p style="color: #402218;"><h4>© Copyright 2021 Amaris Jewellery & CO.</h4> </p>
+</div>
+</section>
+<!-- footer Section Ends Here -->
+
+
+  </body>
+
+</html>
